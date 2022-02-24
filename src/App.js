fa3818b5f3f414076c561caded6401bb68712dff
Ahmed Al Farouq <mediocre23534@gmail.com>
2022-02-24 21:01:45 +0200

    init redux, fetch products, and create navbar, cart, categories pages, and single product page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+} from '@apollo/client';
+import { useSelector } from 'react-redux';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+import AllCategories from './components/allCategories/AllCategories';
+import Clothes from './components/clothesCategory/Clothes';
+import Tech from './components/techCategory/Tech';
+import SingleProduct from './components/singleProduct/SingleProduct';
+import Loading from './components/Loading';
+import Error from './components/Error';
 
-function App() {
+export const CategoriesContext = React.createContext();
+export const CurrenciesContext = React.createContext();
+
+export const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
+
+const App = () => {
+  const loading = useSelector((state) => state.loading);
+  const error = useSelector((state) => state.error);
+
+  if (loading) return <Loading />;
+  if (error) return <Error message={error} />;
+
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<AllCategories />} />
+        <Route path="/clothes" element={<Clothes />} />
+        <Route path="/tech" element={<Tech />} />
+        <Route path="/single-product" element={<SingleProduct />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,45 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql,
-} from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import './index.css';
-import App from './App';
-
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache(),
-});
-
-client
-  .query({
-    query: gql`
-      query {
-        product(id: "jacket-canada-goosee") {
-          id
-          name
-          inStock
-          gallery
-          description
-          category
-          brand
-          prices {
-            amount
-          }
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result.data.product));
+import App, { client } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider>
-      <App />
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root'),
